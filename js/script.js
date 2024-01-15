daaed69c3489cd2d7a4e93cde3fd2276887a7987
Jose A. Raposo <joseantonio.raposo.perez@iesdonana.org>
2024-01-15 19:44:39 +0100

    ligthbox

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+
+const bloque = document.querySelectorAll('.bloque');
+const titulo = document.querySelectorAll('.pregunta');
+
+
+titulo.forEach( (titulos, i )=>{
+    titulo[i].addEventListener('mouseenter', ()=> {
+    
+        bloque.forEach( (bloques, i ) =>{
+            bloque[i].classList.remove('activo');
+        })
+        bloque[i].classList.add('activo');
+    })
+})
+
+const enlaces = document.querySelectorAll('.cont-grupo-imagenes .a');
+const lightbox = document.querySelector('.ligthbox');
+const grande = document.querySelector('.grande');
+
+enlaces.forEach((enlace , n) => {
+    enlaces[n].addEventListener('mouseenter', (e)=>{
+        e.preventDefault();
+        let ruta = enlace.querySelector('.img').src;
+        console.log(ruta);
+
+        lightbox.classList.add('activo');
+        grande.setAttribute('src', ruta);
+    })
+})
